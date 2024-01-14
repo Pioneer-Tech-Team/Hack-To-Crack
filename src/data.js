583f540410ad6824d92d3326817c2447d55e0168
Pratham1606 <144457977+Pratham1606@users.noreply.github.com>
2024-01-14 22:06:57 +0530

    names

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const coordinatorsData = [
     {
-        name: "Ms. Sneha Nimbare ",
+        name: "Ms. Sneha H. Nimbare ",
         position: "Professor - AIML",
         image: "/images/nandhashree.webp",
     },
