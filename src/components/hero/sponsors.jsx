6538993f1b4d9fe04e3c2153201528099ed5a636
Pranav Karawale <pranav@karawale.in>
2024-02-17 12:22:00 +0530

    Add sponsors

--- src/components/hero/sponsors.jsx
+++ src/components/hero/sponsors.jsx
@@ -7,190 +7,206 @@ import Convener from "./convener";
 import Coordinators from "./coordinators";
 import Team from "./team";
 const sponsors = () => {
-  return (
-    <React.Fragment>
-      <div className="parent-sponsors">
-        <h1 className="sponsors-title">
-          Sponsor
-          <img src="/images/rrr.webp" alt="arrow-logo" />
-        </h1>
-      <p className="sponsor-tag golden"><span href="#" >General</span></p>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="POLLUTION CONTROL BOARD OF MAHARASHTRA" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://mpcb.gov.in/node" target="_blank">
-                  {" "}
-                  MAHARASHTRA POLLUTION CONTROL BOARD{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="INDIAN GREEN BUILDING COUNCIL" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://igbc.in/" target="_blank">
-                  {" "}
-                  INDIAN GREEN BUILDING COUNCIL{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="PREM JAIN MEMORIAL TRUST" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://premjainmemorialtrust.com/" target="_blank">
-                  {" "}
-                  PREM JAIN MEMORIAL TRUST{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="APTECH-Panvel" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://aptechpanvel.in/" target="_blank">
-                  {" "}
-                  APTECH-Panvel{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="RKDEMY" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://www.rkdemy.in/" target="_blank">
-                  {" "}
-                  RKDEMY{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="APCHEMI" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://www.apchemi.com/" target="_blank">
-                  {" "}
-                  APCHEMI{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="SONSVELS SOLUTION" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://sonsvels-solution.business.site/" target="_blank">
-                  {" "}
-                  SONSVELS SOLUTION{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="DSES PVT LTD" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://www.dnyandases.com" target="_blank">
-                  {" "}
-                  DSES PVT LTD{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-          <div>
-            <p className="sponsor-tag"><span>Website Sponsor</span></p>
-            <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="Pioneertech" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://pioneertech.net.in/" target="_blank">
-                  {" "}
-                  Pioneertech{" "}
-                </a>
-              </h1>
-            </div>{" "}
-            <LaunchLink className="link-launch-icon" />
-          </div>
-        </div>
-          </div>
+	return (
+		<React.Fragment>
+			<div className="parent-sponsors">
+				<h1 className="sponsors-title">
+					Sponsor
+					<img src="/images/rrr.webp" alt="arrow-logo" />
+				</h1>
+				<p className="sponsor-tag golden">
+					<span href="#">General</span>
+				</p>
+				<div className="sponsors">
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img
+									src="/images/sponsors/mh-pollution-control-board.jpeg"
+									alt="POLLUTION CONTROL BOARD OF MAHARASHTRA"
+								/>
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a href="https://mpcb.gov.in/node" target="_blank">
+										MAHARASHTRA POLLUTION CONTROL BOARD
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img
+									src="/images/sponsors/indian-green-building-council.jpeg"
+									alt="INDIAN GREEN BUILDING COUNCIL"
+								/>
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a href="https://igbc.in/" target="_blank">
+										INDIAN GREEN BUILDING COUNCIL
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img
+									src="/images/sponsors/prem-jain.jpeg"
+									alt="PREM JAIN MEMORIAL TRUST"
+								/>
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a href="https://premjainmemorialtrust.com/" target="_blank">
+										PREM JAIN MEMORIAL TRUST
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img src="/images/sponsors/ap-tech.webp" alt="APTECH-Panvel" />
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a href="https://aptechpanvel.in/" target="_blank">
+										APTECH-Panvel
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img src="/images/sponsors/rkdemy.png" alt="RKDEMY" />
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a href="https://www.rkdemy.in/" target="_blank">
+										RKDEMY
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img src="/images/sponsors/apchemi.jpeg" alt="APCHEMI" />
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a href="https://www.apchemi.com/" target="_blank">
+										APCHEMI
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img
+									src="/images/sponsors/sonsvels.jpg"
+									alt="SONSVELS SOLUTION"
+								/>
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a
+										href="https://sonsvels-solution.business.site/"
+										target="_blank"
+									>
+										SONSVELS SOLUTION
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+					<div className="sponsors-container">
+						<div className="sponsors-border">
+							<div>
+								<img src="/images/sponsors/dses.webp" alt="DSES PVT LTD" />
+							</div>
+							<div className="sponsors-container-sub">
+								<h1 className="sponsor-title">
+									<a href="https://www.dnyandases.com" target="_blank">
+										DSES PVT LTD
+									</a>
+								</h1>
+							</div>
+							<LaunchLink className="link-launch-icon" />
+						</div>
+					</div>
+				</div>
+				<div className="sponsors">
+					<div>
+						<p className="sponsor-tag">
+							<span>Website Sponsor</span>
+						</p>
+						<div className="sponsors-container">
+							<div className="sponsors-border">
+								<div>
+									<img
+										src="/images/sponsors/pioneertech.png"
+										alt="Pioneertech"
+									/>
+								</div>
+								<div className="sponsors-container-sub">
+									<h1 className="sponsor-title">
+										<a href="https://pioneertech.net.in/" target="_blank">
+											Pioneertech
+										</a>
+									</h1>
+								</div>
+								<LaunchLink className="link-launch-icon" />
+							</div>
+						</div>
+					</div>
 
-          <div>
-            <p className="sponsor-tag"><span>T-shirt Sponsor</span></p>
-            <div className="sponsors-container">
-          <div className="sponsors-border">
-            <div>
-              <img src="/images/pioneertech.png" alt="JUSTINKIT" />
-            </div>
-            <div className="sponsors-container-sub">
-              <h1 className="sponsor-title">
-                <a href="https://" target="_blank">
-                  {" "}
-                  JUSTINKIT{" "}
-                </a>
-              </h1>
-            </div>{" "}
-          </div>
-        </div>
-          </div>
-        </div>
-        <Faqs />
-        <Contact />
-        <Convener />
-        <Coordinators />
-        {/* <Team /> */}
-      </div>
-    </React.Fragment>
-  );
+					<div>
+						<p className="sponsor-tag">
+							<span>T-shirt Sponsor</span>
+						</p>
+						<div className="sponsors-container">
+							<div className="sponsors-border">
+								<div>
+									<img src="/images/sponsors/justinkit.png" alt="JUSTINKIT" />
+								</div>
+								<div className="sponsors-container-sub">
+									<h1 className="sponsor-title">
+										<a href="https://" target="_blank">
+											JUSTINKIT
+										</a>
+									</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<Faqs />
+				<Contact />
+				<Convener />
+				<Coordinators />
+				{/* <Team /> */}
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default sponsors;
