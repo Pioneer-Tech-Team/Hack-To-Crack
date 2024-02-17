fddbc7988e9788ec226ff6931457f94592de6f4d
Pranav Karawale <pranav@karawale.in>
2024-02-17 15:18:32 +0530

    Add convenor message

--- src/components/hero/convener.jsx
+++ src/components/hero/convener.jsx
@@ -3,32 +3,35 @@ import "./convener.css";
 import { convenerData } from "../../data";
 
 const convener = () => {
-  return (
-    <React.Fragment>
-      <div className="parent-convener">
-        <div className="convener-title">
-          <img src="/images/rrr.webp" alt="" className="convener-logo" />
-          <h1 className="convener-title-name">Convener</h1>
-        </div>
+	return (
+		<React.Fragment>
+			<div className="parent-convener">
+				<div className="convener-title">
+					<img src="/images/rrr.webp" alt="" className="convener-logo" />
+					<h1 className="convener-title-name">Convener</h1>
+				</div>
 
-        <div className="convener-container">
-          {convenerData.map((data, index) => {
-            return (
-              <div className="convener-card" key={index}>
-                <img
-                  className="convener-card-image"
-                  src={data.image}
-                  alt={data.name}
-                />
-                <h1 className="convener-name">{data.name}</h1>
-                <h1 className="convener-position">{data.position}</h1>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </React.Fragment>
-  );
+				<div className="convener-container">
+					{convenerData.map((data, index) => {
+						return (
+							<div className="convener" key={index}>
+								<div className="convener-card" key={index}>
+									<img
+										className="convener-card-image"
+										src={data.image}
+										alt={data.name}
+									/>
+									<h1 className="convener-name">{data.name}</h1>
+									<h1 className="convener-position">{data.position}</h1>
+								</div>
+								<blockquote className="convener-message">{data.message}</blockquote>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default convener;
